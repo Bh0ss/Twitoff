19c3289c692a0d39572d3703c51db37dce0e8cbe
bh0ss <hossbrendan@gmail.com>
2020-10-15 13:02:57 -0400

    Almost ready for deployment

--- twitoff/models.py
+++ twitoff/models.py
@@ -1,40 +1,27 @@
-"""SQLAlchemy models and utility fucntions for TwitOff"""
-
+"""SQLAlchemy models and utility functions for TwitOff."""
 from flask_sqlalchemy import SQLAlchemy
-
 
 DB = SQLAlchemy()
 
 
-# User table with columns id and name
 class User(DB.Model):
-  """Twitter Users corresponding to Tweets"""
-  id = DB.Column(DB.BigInteger, primary_key=True)
-  name = DB.Column(DB.String, nullable=False)
-  newest_tweet_id = DB.Column(DB.BigInteger)
+    """Twitter users corresponding to Tweets in the Tweet table."""
+    id = DB.Column(DB.BigInteger, primary_key=True)
+    name = DB.Column(DB.String(15), nullable=False)
+    # Tweet IDs are ordinal ints, so can be used to fetch only more recent
+    newest_tweet_id = DB.Column(DB.BigInteger)
 
-  def __repr__(self):
-    return "<User: {}>".format(self.name)
+    def __repr__(self):
+        return '<User {}>'.format(self.name)
 
 
-# Tweet table with columns id, text, and user_id
 class Tweet(DB.Model):
-  """Tweet related to a user"""
-  id = DB.Column(DB.BigInteger, primary_key=True)
-  text = DB.Column(DB.Unicode(300))
-  vect = DB.Column(DB.PickleType, nullable=False)
-  user_id = DB.Column(DB.BigInteger, DB.ForeignKey('user.id'), nullable=False)
-  user = DB.relationship("User", backref=DB.backref("tweets", lazy=True))
+    """Tweets and their embeddings from Basilica."""
+    id = DB.Column(DB.BigInteger, primary_key=True)
+    text = DB.Column(DB.Unicode(300))  # Allowing for full + links
+    embedding = DB.Column(DB.PickleType, nullable=False)
+    user_id = DB.Column(DB.BigInteger, DB.ForeignKey('user.id'), nullable=False)
+    user = DB.relationship('User', backref=DB.backref('tweets', lazy=True))
 
-  def __repr__(self):
-    return "<Tweet: {}>".format(self.text)
-
-
-# example users but remember they dont have tweets
-def insert_example_users():
-  """ Example users """
-  bill = User(id=1, name="BillGates")
-  elon = User(id=2, name="ElonMusk")
-  DB.session.add(bill)
-  DB.session.add(elon)
-  DB.session.commit()
+    def __repr__(self):
+        return '<Tweet {}>'.format(self.text)
